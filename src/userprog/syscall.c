8e0117aa6070d3c368d382b9b85e66f04a354252
Zihao-Ye <70185800+Zihao-Ye@users.noreply.github.com>
2021-11-24 16:03:23 +0800

    添加exec、write、close部分注释

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,67 +4,79 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
-static void syscall_handler (struct intr_frame *);
+static void syscall_handler(struct intr_frame *);
 
-void
-syscall_init (void) 
+void syscall_init(void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler(struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  printf("system call!\n");
+  thread_exit();
 }
-struct fd*
+
+//根据fd值获取fd
+struct fd *
 get_fd_by_code(int fd_code)
 {
   struct list_elem *e;
   struct thread *cur = thread_current();
-  for (e = list_begin (&cur->fd_list); e != list_end (&cur->fd_list); e = e->prev)
+  //遍历当前线程的fd列表，查找符合的对象
+  for (e = list_begin(&cur->fd_list); e != list_end(&cur->fd_list); e = e->prev)
   {
-    struct fd *fd = list_entry (e, struct fd, fd_elem);
-    if(fd->fd_code == fd_code)return fd;
+    struct fd *fd = list_entry(e, struct fd, fd_elem);
+    if (fd->fd_code == fd_code)
+      return fd;
   }
   return NULL;
 }
 
-void
-exit(int status){
+void exit(int status)
+{
   struct list_elem *e;
-  struct thread *cur = thread_current ();
+  struct thread *cur = thread_current();
+  //将线程的所有文件关闭
   while (!list_empty(&cur->fd_list))
   {
     e = list_begin(&cur->fd_list);
     close(list_entry(e, struct fd, fd_elem)->fd_code);
   }
-  file_close(cur->opened_file);
+  file_close(cur->dealing_file);
+  //设置线程退出码为指定退出码
   thread_current()->exit_code = status;
   thread_exit();
 }
-int
-write(int fd_code,const void* buffer,unsigned size){
-  
+int write(int fd_code, const void *buffer, unsigned size)
+{
+
   if (fd_code == STDOUT_FILENO)
   {
+    //输出到控制台
     putbuf((const char *)buffer, size);
     return size;
   }
 
   struct fd *f = get_fd_by_code(fd_code);
-  if(f==NULL)return -1;
+  if (f == NULL)
+    return -1;
+  //向文件进行写，得到返回码，即实际写入的字节数
   int ret = file_write(f->file, buffer, size);
 
   return ret;
 }
-void
-close(int fd_code)
+
+//通过文件标识符的值关闭文件
+void close(int fd_code)
 {
-    struct fd *f = get_fd_by_code(fd_code);
-    if(f==NULL)return -1;
-    file_close(f->file);
-    list_remove(&f->fd_elem);
-    free(f);
+  //根据fd值获取文件标识符
+  struct fd *f = get_fd_by_code(fd_code);
+  if (f == NULL)
+    return -1;
+  //关闭文件
+  file_close(f->file);
+  list_remove(&f->fd_elem);
+  free(f);
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -4,14 +4,18 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-void syscall_init (void);
+void syscall_init(void);
 
 #endif /* userprog/syscall.h */
-struct fd{
-    int fd_code;
-    struct file* file;
-    struct list_elem fd_elem;
-
+//文件描述符
+struct fd
+{
+    int fd_code;              //文件描述符的值
+    struct file *file;        //该描述符指向的文件
+    struct list_elem fd_elem; //代表fd的对象
 };
-void exit (int);
-int write(int,const void *,unsigned);
+//退出
+void exit(int);
+
+//写
+int write(int, const void *, unsigned);
