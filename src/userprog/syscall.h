6bedf0c7d4e1d3f7d3dfc5b02b27ccde23dff2d5
ashen706 <ashen706@163.com>
2021-12-04 15:30:18 +0800

    open

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,12 +1,11 @@
-#include <list.h>
+#include <lib/kernel/list.h>
 #include <filesys/file.h>
 #include <threads/synch.h>
 
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
-
+typedef int pid_t;
+struct lock file_lock;
 void syscall_init(void);
-
+void exit(int);
 #endif /* userprog/syscall.h */
-
-
